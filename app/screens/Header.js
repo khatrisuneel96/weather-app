88f05e04cb0fc13817a21dc3cc70799400563aa5
Suneel Kumar Khatri <khatrisuneel96@gmail.com>
2020-09-08 20:04:04 +0500

    Header color

--- app/screens/Header.js
+++ app/screens/Header.js
@@ -4,7 +4,7 @@ import { Appbar } from "react-native-paper";
 
 export default function Header() {
   return (
-    <Appbar.Header>
+    <Appbar.Header style={{ backgroundColor: "#1C9CF6" }}>
       <Appbar.Content
         titleStyle={{
           fontFamily: "Century-Gothic-Bold",
