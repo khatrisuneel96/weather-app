110a38a46f0ed442dfb62a63816a44a89eb0517c
Suneel Kumar Khatri <khatrisuneel96@gmail.com>
2020-09-08 20:00:03 +0500

    App bar

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { useFonts } from "expo-font";
+import { AppLoading } from "expo";
+import { StyleSheet, Text, View } from "react-native";
+import Header from "./app/screens/Header";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [fontLoaded] = useFonts({
+    "Century-Gothic-Regular": require("./app/assets/fonts/Century-Gothic-Regular.ttf"),
+    "Century-Gothic-Bold": require("./app/assets/fonts/Century-Gothic-Bold.ttf"),
+  });
+
+  if (!fontLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={styles.container}>
+        <Header />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
---
+++ app/screens/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Appbar } from "react-native-paper";
+
+export default function Header() {
+  return (
+    <Appbar.Header>
+      <Appbar.Content
+        titleStyle={{
+          fontFamily: "Century-Gothic-Bold",
+          fontSize: 25,
+          textAlign: "center",
+        }}
+        subtitleStyle={{
+          fontFamily: "Century-Gothic-Regular",
+          fontSize: 13,
+          textAlign: "center",
+        }}
+        title="Weather App"
+        subtitle="Find you weather info!"
+      />
+    </Appbar.Header>
+  );
+}
+
+const styles = StyleSheet.create({});
