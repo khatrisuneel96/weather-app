38489524af60636e93124c2068c0fcddb1eeb99e
Suneel Kumar Khatri <khatrisuneel96@gmail.com>
2020-09-09 20:36:55 +0500

    Final

--- App.js
+++ App.js
@@ -2,7 +2,12 @@ import { StatusBar } from "expo-status-bar";
 import React, { Component } from "react";
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
-import { StyleSheet, SafeAreaView, ActivityIndicator } from "react-native";
+import {
+  StyleSheet,
+  SafeAreaView,
+  ActivityIndicator,
+  ScrollView,
+} from "react-native";
 import Header from "./app/screens/Header";
 import Content from "./app/screens/Content";
 import Footer from "./app/screens/Footer";
@@ -55,15 +60,17 @@ export default class App extends Component {
     }
     if (this.state.fontsLoaded) {
       return (
-        <SafeAreaView style={styles.container}>
-          <Header
-            data={[this.state.report.list[0], this.state.report.detail]}
-          />
-          <Content data={this.state.report.list} />
-          <Footer data={this.state.report.list[0]} />
+        <ScrollView>
+          <SafeAreaView style={styles.container}>
+            <Header
+              data={[this.state.report.list[0], this.state.report.detail]}
+            />
+            <Content data={this.state.report.list} />
+            <Footer data={this.state.report.list[0]} />
 
-          <StatusBar style="auto" />
-        </SafeAreaView>
+            <StatusBar style="auto" />
+          </SafeAreaView>
+        </ScrollView>
       );
     } else {
       return <AppLoading />;
--- app/screens/Content.js
+++ app/screens/Content.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import Card from "./card/Card";
 
 export default class Content extends Component {
